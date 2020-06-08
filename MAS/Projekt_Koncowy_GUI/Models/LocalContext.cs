9f8878df97141b003d7ba87cef9320b502da002a
Mateusz <mateusz@o2.pl>
2020-06-08 15:19:03 +0200

    ConnectionString

---
+++ MAS/Projekt_Koncowy_GUI/Models/LocalContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Koncowy_GUI.Models
+{
+    public class LocalContext
+    {
+    }
+}
