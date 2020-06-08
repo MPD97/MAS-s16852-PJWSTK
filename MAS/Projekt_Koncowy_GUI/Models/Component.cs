1621a441ebe7473b48e6e3db5e512233eb4a5d97
Mateusz <mateusz@o2.pl>
2020-06-08 15:21:49 +0200

    Implementing replacement

---
+++ MAS/Projekt_Koncowy_GUI/Models/Component.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Koncowy_GUI.Models
+{
+    public class Component 
+    {
+        public Guid ComponentId { get; set; }
+
+        public Guid Identifier { get; set; }
+
+        public Component(Guid identifier) 
+        {
+            Identifier = identifier;
+        }
+
+        public bool Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Replacement
+    {
+        public Guid ReplacementId { get; set; }
+
+        public Guid BaseId { get; set; }
+        public Guid ReplacedById { get; set; }
+
+        [ForeignKey("BaseId")]
+        public Component Base { get; set; }
+
+        [ForeignKey("ReplacedById")]
+        public Component ReplacedBy { get; set; }
+    }
+}
--- MAS/Projekt_Koncowy_GUI/Models/LocalContext.cs
+++ MAS/Projekt_Koncowy_GUI/Models/LocalContext.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Projekt_Koncowy_GUI.Models
 {
-    public class LocalContext
+    public class LocalContext : DbContext
     {
     }
 }
