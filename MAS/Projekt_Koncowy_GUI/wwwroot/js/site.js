db8c99ddab26d7c35a9de346fd6c976f366a80a7
Mateusz <mateusz@o2.pl>
2020-06-09 15:37:52 +0200

    Repair with view 2/3

--- MAS/Projekt_Koncowy_GUI/wwwroot/js/site.js
+++ MAS/Projekt_Koncowy_GUI/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var btn = document.getElementById("CheckBtn");
+btn.onclick = function () {
+
+    var selectors = document.querySelectorAll("select");
+
+    selectors.forEach(element => {
+        if (element.selectedIndex === 0) {
+            alert("Wybierz opcję Koniecznosć wymiany!");
+            return;
+        }
+    });
+
+    selectors.forEach(element => {
+        if (element.selectedIndex === 1) {
+
+        }
+    });
+};
