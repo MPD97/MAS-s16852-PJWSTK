42cab1c1997a9151ed7f094225b777c0fc201a56
Mateusz <mateusz@o2.pl>
2020-07-03 08:58:05 +0200

    class Osoba implemented

---
+++ MAS/ZPR-7/Osoba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ZPR_7
+{
+    public abstract class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Email { get; set; }
+        public string Haslo { get; set; }
+        public string Avatar { get; set; } //Scieżka do pliku
+
+        public abstract void UstawAwatar();
+    }
+}
