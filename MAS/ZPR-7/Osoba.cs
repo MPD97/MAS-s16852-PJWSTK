74d0752134f2269a68ecbe87a9e45cdb0207fa69
Mateusz <mateusz@o2.pl>
2020-07-03 09:09:23 +0200

    Pracownik & UzytkonikZarejestrowany implemented

--- MAS/ZPR-7/Osoba.cs
+++ MAS/ZPR-7/Osoba.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using MP03.Functional;
 using System.Collections.Generic;
 using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace ZPR_7
 {
-    public abstract class Osoba
+    public abstract class Osoba : ObjectPlusPlus
     {
         public string Imie { get; set; }
         public string Nazwisko { get; set; }
         public string Email { get; set; }
         public string Haslo { get; set; }
         public string Avatar { get; set; } //Scieżka do pliku
+
+        public Osoba() : base()
+        {
+
+        }
 
         public abstract void UstawAwatar();
     }
---
+++ MAS/ZPR-7/UżytkownikZarejestrowany.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZPR_7
+{
+    public class UżytkownikZarejestrowany : Osoba
+    {
+        public string Pseudonim { get; set; }
+        public long Ranga { get; set; }
+
+        public UżytkownikZarejestrowany() : base()
+        {
+
+        }
+
+        public override void UstawAwatar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
